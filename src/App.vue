31aafcab86e176a5f0eedb466db4946a15692d53
lucaperini <luca.perini@gmail.com>
2022-03-29 19:36:36 +0200

    implementazione sezione main

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,6 @@ export default {
 
 <style lang="scss">
 @import "./assets/scss/style.css";
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
 
 #app {
---
+++ src/components/IndexMainComics.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="comic-element">
+        <img :src="comicObject.thumb" :alt="comicObject.series">
+        <h4>{{comicObject.series}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexMainComics",
+    props: ['comicObject'],
+
+}
+</script>
+
+<style lang="scss" scoped>
+.comic-element {
+    width:calc((90% / 6) - 10px);
+    display:inline-block;
+    
+    img {
+        width: 100px;
+        height: 100px;
+        object-fit:cover;
+    }
+    h4{
+        font-size:.7rem;
+        margin-top:.5rem;
+        }
+}
+</style>
